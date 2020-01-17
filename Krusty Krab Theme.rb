b303660e809971574a0f90d2da9fabc60843e727
LeonardoPahtle <45046585+LeonardoPahtle@users.noreply.github.com>
2020-01-17 14:09:13 -0800

    Add files via upload

---
+++ Krusty Krab Theme.rb
@@ -0,0 +1,141 @@
+# Welcome to Sonic Pi v3.1
+use_bpm 160
+3.times do
+  play:Cs5
+  sleep 1
+  play:A4
+  sleep 0.5
+  play:E4
+  sleep 1
+  play:A4
+  sleep 0.5
+  play:Cs5
+  sleep 1
+  play:A4
+  sleep 0.5
+  play:E5
+  sleep 1
+  play:Cs5
+  sleep 0.5
+  play:A5
+  sleep 0.5
+  play:Gs5
+  sleep 0.5
+  play:A5
+  sleep 0.5
+  play:B5
+  sleep 0.5
+  play:A5
+  sleep 0.5
+  play:Gs5
+  sleep 0.5
+  play:A5
+  sleep 1
+  play:E5
+  sleep 0.5
+  play:Cs5
+  sleep 1
+  play:A4
+  sleep 0.5
+  play:Gs4
+  sleep 1
+  play:Fs4
+  sleep 0.5
+  play:Gs4
+  sleep 1
+  play:A4
+  sleep 0.5
+  play:B4
+  sleep 1
+  play:Cs5
+  sleep 0.5
+  play:D5
+  sleep 0.5
+  play:Cs5
+  sleep 0.5
+  play:B4
+  sleep 0.5
+  play:E5
+  sleep 1
+  play:Ds5
+  sleep 0.5
+  play:E5
+  sleep 1
+  play:Fs5
+  sleep 0.5
+  play:Gs5
+  sleep 1
+  play:E5
+  sleep 0.5
+  play:D5
+  sleep 1
+  play:B4
+  sleep 0.5
+end
+
+play:Cs5
+sleep 1
+play:A4
+sleep 0.5
+play:E4
+sleep 1
+play:A4
+sleep 0.5
+play:Cs5
+sleep 1
+play:A4
+sleep 0.5
+play:E5
+sleep 1
+play:Cs5
+sleep 0.5
+play:A5
+sleep 0.5
+play:Gs5
+sleep 0.5
+play:A5
+sleep 0.5
+play:B5
+sleep 0.5
+play:A5
+sleep 0.5
+play:Gs5
+sleep 0.5
+play:A5
+sleep 1
+play:E5
+sleep 0.5
+play:Cs5
+sleep 1
+play:A4
+sleep 0.5
+play:Cs5
+sleep 0.5
+play:D5
+sleep 0.5
+play:E5
+sleep 0.5
+play:A5
+sleep 0.5
+play:E5
+sleep 0.5
+play:D5
+sleep 0.5
+play:D5
+sleep 1
+play:B4
+sleep 0.5
+play:Gs4
+sleep 1
+play:A4
+sleep 0.5
+play:B4
+sleep 1
+play:A4
+sleep 0.5
+play:A4
+sleep 1
+play:Gs4
+sleep 0.5
+play:A4
+sleep 3
