5efb01254d58636a6a5782146910e13fd48b787d
LeonardoPahtle <45046585+LeonardoPahtle@users.noreply.github.com>
2020-01-17 14:03:27 -0800

    Add files via upload

---
+++ Stranger Things.rb
@@ -0,0 +1,21 @@
+# Welcome to Sonic Pi v3.1
+use_bpm 160
+live_loop:name do
+  use_synth:prophet
+  play:C4
+  sleep 0.5
+  play:E4
+  sleep 0.5
+  play:G4
+  sleep 0.5
+  play:B4
+  sleep 0.5
+  play:C5
+  sleep 0.5
+  play:B4
+  sleep 0.5
+  play:G4
+  sleep 0.5
+  play:E4
+  sleep 0.5
+end
