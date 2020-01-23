b1cd0d6759583a9af04efaa96544cf381a4043e6
LeonardoPahtle <45046585+LeonardoPahtle@users.noreply.github.com>
2020-01-23 11:55:24 -0800

    Add files via upload

---
+++ Spongebob Ending Theme.rb
@@ -0,0 +1,322 @@
+# Welcome to Sonic Pi v3.1
+use_bpm 136
+use_synth :piano
+live_loop:Notes do
+  14.times do
+    play:E3
+    sleep 1
+    play:B3
+    play:E4
+    sleep 1
+    play:B3
+    sleep 1
+    play:E4
+    sleep 1
+  end
+  stop
+end
+
+sleep 1
+play:E4
+play:Gs4
+sleep 2
+play:E5
+play:Gs4
+sleep 1
+play:E4
+sleep 1
+play:E5
+play:Gs4
+sleep 2
+play:E5
+play:Gs4
+sleep 2
+play:E5
+play:Gs4
+play:B4
+sleep 1
+play:B4
+sleep 1
+play:Gs4
+play:E5
+sleep 2
+play:E5
+play:Gs4
+play:B4
+sleep 1
+play:B4
+sleep 1
+play:Gs4
+play:E5
+sleep 1
+play:B4
+sleep 0.75
+play:As4
+sleep 0.25
+play:Gs4
+play:B4
+sleep 0.75
+play:Cs5
+sleep 0.25
+play:B4
+sleep 1
+play:Gs4
+play:E5
+sleep 2
+play:E5
+play:Gs4
+play:B4
+sleep 1
+play:B4
+sleep 1
+play:Gs4
+play:E5
+sleep 1
+play:B4
+sleep 0.75
+play:As4
+sleep 0.25
+play:Gs4
+play:B4
+sleep 0.75
+play:Cs5
+sleep 0.25
+play:B4
+sleep 1
+play:Gs4
+play:E5
+sleep 2
+play:E5
+play:Gs4
+play:B4
+sleep 1
+play:B4
+sleep 1
+play:Gs4
+play:E5
+sleep 1
+play:B4
+sleep 0.75
+play:As4
+sleep 0.25
+play:Gs4
+play:B4
+sleep 0.75
+play:Cs5
+sleep 0.25
+play:B4
+sleep 1
+play:Gs4
+play:E4
+play:E5
+sleep 1
+play:B4
+sleep 1
+play:Gs4
+play:E5
+sleep 2
+play:Gs4
+play:E5
+sleep 1
+play:B4
+sleep 0.75
+play:As4
+sleep 0.25
+play:Gs4
+play:B4
+sleep 0.75
+play:Cs5
+sleep 0.25
+play:B4
+sleep 1
+play:Gs4
+play:E5
+sleep 2
+play:E5
+play:Gs4
+play:B4
+sleep 1
+play:B4
+sleep 1
+play:Gs4
+play:E5
+sleep 1
+play:B4
+sleep 0.75
+play:As4
+sleep 0.25
+play:Gs4
+play:B4
+sleep 0.75
+play:Cs5
+sleep 0.25
+play:B4
+sleep 1
+play:Gs4
+play:E5
+sleep 0.5
+play:B4
+sleep 1.5
+play:E5
+play:B4
+play:Gs4
+sleep 1
+play:E5
+play:B4
+play:Gs4
+sleep 2
+
+live_loop:Notes2 do
+  1.times do
+    play:Ds3
+    play:Gs3
+    play:B3
+    play:Ds4
+    sleep 1
+    play:Ds3
+    play:Gs3
+    play:B3
+    play:Ds4
+    sleep 1
+    play:Ds3
+    play:Gs3
+    play:B3
+    play:Ds4
+    sleep 2
+    play:E3
+    sleep 1
+    play:B3
+    play:E4
+    sleep 1
+    play:B3
+    sleep 1
+    play:E4
+    sleep 1
+    play:E3
+    play:A3
+    play:Cs4
+    play:E4
+    sleep 1
+    play:E3
+    play:A3
+    play:C4
+    sleep 1
+    play:E3
+    play:A3
+    play:Cs4
+    sleep 2
+    play:E3
+    sleep 1
+    play:B3
+    play:E4
+    sleep 1
+    play:B3
+    sleep 1
+    play:E4
+    sleep 1
+    play:B3
+    sleep 1
+    play:Cs4
+    sleep 3
+    play:E3
+    sleep 1
+    play:B3
+    play:E4
+    sleep 1
+    play:B3
+    sleep 1
+    play:E4
+    sleep 1
+    play:E3
+    sleep 4
+  end
+  stop
+end
+
+play:B4
+play:Fs5
+sleep 0.75
+play:Gs5
+sleep 0.25
+play:B4
+play:Fs5
+sleep 0.75
+play:Gs5
+sleep 0.25
+play:Gs4
+play:B4
+play:Fs5
+sleep 2
+play:Gs4
+play:B4
+play:Fs5
+sleep 1
+play:Gs4
+play:B4
+play:E5
+sleep 1
+play:B4
+play:E5
+sleep 1
+play:G4
+play:E5
+sleep 0.5
+play:Fs5
+sleep 0.5
+play:A4
+play:E5
+play:G5
+sleep 1
+play:A4
+play:E5
+play:G5
+sleep 1
+play:A4
+play:E5
+play:G5
+sleep 0.5
+play:Fs5
+sleep 0.5
+play:E5
+sleep 0.5
+play:B4
+sleep 0.5
+play:B4
+sleep 0.75
+play:As4
+sleep 0.25
+play:Gs4
+play:B4
+sleep 0.75
+play:Cs5
+sleep 0.25
+play:B4
+sleep 1
+play:Gs4
+sleep 1
+play:B4
+sleep 0.75
+play:As4
+sleep 0.25
+play:A4
+play:Fs5
+sleep 0.75
+play:Cs5
+sleep 0.25
+play:B4
+sleep 1
+play:A4
+play:Ds5
+play:Fs5
+sleep 1
+play:Ds5
+sleep 1
+play:Gs4
+play:B4
+play:E5
+sleep 1
+play:B4
+sleep 1
+play:Gs4
+play:E5
+sleep 5
