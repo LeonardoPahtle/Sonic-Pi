0281ff9cf6b349cf7765996b0810c05b0fa27484
LeonardoPahtle <45046585+LeonardoPahtle@users.noreply.github.com>
2020-02-06 11:58:34 -0800

    Add files via upload

---
+++ Spongebob Ending Theme Requirements.rb
@@ -0,0 +1,354 @@
+# Welcome to Sonic Pi v3.1
+patrick = "C:/Users/leonardo_quechol/Downloads/Patrick Explaining Wumbo.wav"
+Patrick = "C:/Users/leonardo_quechol/Downloads/Three Hours Later (Patrick) SpongeBob Time Card #23.wav"
+Hi = "C:/Users/leonardo_quechol/Downloads/Hi How Are Ya - Spongebob Sound Effect (HD).wav"
+What = "C:/Users/leonardo_quechol/Downloads/You what! SpongeBob sound effect.wav"
+Leg = "C:/Users/leonardo_quechol/Downloads/spongebob my leg sound effect.wav"
+Evil = "C:/Users/leonardo_quechol/Downloads/Mermaid man - evil.wav"
+Note = [:E3, :E4, :B3, :E4]
+x = 0.5
+i = 0
+
+use_bpm 136
+use_synth :piano
+
+define :amp do
+  if x < 1
+    print "no"
+  else
+    x = 1
+  end
+end
+
+sample patrick
+sleep 60
+
+live_loop:Notes do
+  14.times do
+    4.times do
+      play Note[i], amp: x
+      print x
+      sleep 1
+      i = i + 1
+      x = x + 0.1
+      amp
+    end
+    i = 0
+  end
+  stop
+end
+
+live_loop:Sounds do
+  sleep 16
+  4.times do
+    sleep 3
+    sample choose([Evil, Hi, What, Leg, What])
+    sleep 5
+  end
+  stop
+end
+
+sleep 1
+play:E4, amp: x
+play:Gs4, amp: x
+sleep 2
+play:E5, amp: x
+play:Gs4, amp: x
+sleep 1
+play:E4, amp: x
+sleep 1
+play:E5, amp: x
+play:Gs4, amp: x
+sleep 2
+play:E5, amp: x
+play:Gs4, amp: x
+sleep 2
+play:E5, amp: x
+play:Gs4, amp: x
+play:B4, amp: x
+sleep 1
+play:B4, amp: x
+sleep 1
+play:Gs4, amp: x
+play:E5, amp: x
+sleep 2
+play:E5, amp: x
+play:Gs4, amp: x
+play:B4, amp: x
+sleep 1
+play:B4, amp: x
+sleep 1
+play:Gs4, amp: x
+play:E5, amp: x
+sleep 1
+play:B4, amp: x
+sleep 0.75
+play:As4, amp: x
+sleep 0.25
+play:Gs4, amp: x
+play:B4, amp: x
+sleep 0.75
+play:Cs5, amp: x
+sleep 0.25
+play:B4, amp: x
+sleep 1
+play:Gs4, amp: x
+play:E5, amp: x
+sleep 2
+play:E5, amp: x
+play:Gs4, amp: x
+play:B4, amp: x
+sleep 1
+play:B4, amp: x
+sleep 1
+play:Gs4, amp: x
+play:E5, amp: x
+sleep 1
+play:B4, amp: x
+sleep 0.75
+play:As4, amp: x
+sleep 0.25
+play:Gs4, amp: x
+play:B4, amp: x
+sleep 0.75
+play:Cs5, amp: x
+sleep 0.25
+play:B4, amp: x
+sleep 1
+play:Gs4, amp: x
+play:E5, amp: x
+sleep 2
+play:E5, amp: x
+play:Gs4, amp: x
+play:B4, amp: x
+sleep 1
+play:B4, amp: x
+sleep 1
+play:Gs4, amp: x
+play:E5, amp: x
+sleep 1
+play:B4, amp: x
+sleep 0.75
+play:As4, amp: x
+sleep 0.25
+play:Gs4, amp: x
+play:B4, amp: x
+sleep 0.75
+play:Cs5, amp: x
+sleep 0.25
+play:B4, amp: x
+sleep 1
+play:Gs4, amp: x
+play:E4, amp: x
+play:E5, amp: x
+sleep 2
+play:Gs4, amp: x
+play:E5, amp: x
+sleep 2
+play:Gs4, amp: x
+play:E5, amp: x
+sleep 1
+play:B4, amp: x
+sleep 0.75
+play:As4, amp: x
+sleep 0.25
+play:Gs4, amp: x
+play:B4, amp: x
+sleep 0.75
+play:Cs5, amp: x
+sleep 0.25
+play:B4, amp: x
+sleep 1
+play:Gs4, amp: x
+play:E5, amp: x
+sleep 2
+play:E5, amp: x
+play:Gs4, amp: x
+play:B4, amp: x
+sleep 1
+play:B4, amp: x
+sleep 1
+play:Gs4, amp: x
+play:E5, amp: x
+sleep 1
+play:B4, amp: x
+sleep 0.75
+play:As4, amp: x
+sleep 0.25
+play:Gs4, amp: x
+play:B4, amp: x
+sleep 0.75
+play:Cs5, amp: x
+sleep 0.25
+play:B4, amp: x
+sleep 1
+play:Gs4, amp: x
+play:E5, amp: x
+sleep 0.5
+play:B4, amp: x
+sleep 1.5
+play:E5, amp: x
+play:B4, amp: x
+play:Gs4, amp: x
+sleep 1
+play:E5, amp: x
+play:B4, amp: x
+play:Gs4, amp: x
+sleep 2
+
+live_loop:Notes2 do
+  1.times do
+    play:Ds3
+    play:Gs3
+    play:B3
+    play:Ds4
+    sleep 1
+    play:Ds3
+    play:Gs3
+    play:B3
+    play:Ds4
+    sleep 1
+    play:Ds3
+    play:Gs3
+    play:B3
+    play:Ds4
+    sleep 2
+    play:E3
+    sleep 1
+    play:B3
+    play:E4
+    sleep 1
+    play:B3
+    sleep 1
+    play:E4
+    sleep 1
+    play:E3
+    play:A3
+    play:Cs4
+    play:E4
+    sleep 1
+    play:E3
+    play:A3
+    play:C4
+    sleep 1
+    play:E3
+    play:A3
+    play:Cs4
+    sleep 2
+    play:E3
+    sleep 1
+    play:B3
+    play:E4
+    sleep 1
+    play:B3
+    sleep 1
+    play:E4
+    sleep 1
+    play:B3
+    sleep 1
+    play:Cs4
+    sleep 3
+    play:E3
+    sleep 1
+    play:B3
+    play:E4
+    sleep 1
+    play:B3
+    sleep 1
+    play:E4
+    sleep 1
+    play:E3
+    sleep 4
+  end
+  stop
+end
+
+play:B4
+play:Fs5
+sleep 0.75
+play:Gs5
+sleep 0.25
+play:B4
+play:Fs5
+sleep 0.75
+play:Gs5
+sleep 0.25
+play:Gs4
+play:B4
+play:Fs5
+sleep 2
+play:Gs4
+play:B4
+play:Fs5
+sleep 1
+play:Gs4
+play:B4
+play:E5
+sleep 1
+play:B4
+play:E5
+sleep 1
+play:G4
+play:E5
+sleep 0.5
+play:Fs5
+sleep 0.5
+play:A4
+play:E5
+play:G5
+sleep 1
+play:A4
+play:E5
+play:G5
+sleep 1
+play:A4
+play:E5
+play:G5
+sleep 0.5
+play:Fs5
+sleep 0.5
+play:E5
+sleep 0.5
+play:B4
+sleep 0.5
+play:B4
+sleep 0.75
+play:As4
+sleep 0.25
+play:Gs4
+play:B4
+sleep 0.75
+play:Cs5
+sleep 0.25
+play:B4
+sleep 1
+play:Gs4
+sleep 1
+play:B4
+sleep 0.75
+play:As4
+sleep 0.25
+play:A4
+play:Fs5
+sleep 0.75
+play:Cs5
+sleep 0.25
+play:B4
+sleep 1
+play:A4
+play:Ds5
+play:Fs5
+sleep 1
+play:Ds5
+sleep 1
+play:Gs4
+play:B4
+play:E5
+sleep 1
+play:B4
+sleep 1
+play:Gs4
+play:E5
+sleep 3
+
+sample Patrick
